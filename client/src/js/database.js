6c9e3137cdeba2c5d798346a844854e9b0285adc
Sean Hayes <hayessea000@outlook.com>
2023-05-31 16:21:50 -0500

    getDB finshed

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -27,16 +27,14 @@ export const putDb = async (content) => {
   console.log('🚀 - data saved to the database', result.value);
 };
 
-/*
-  We need to add some code below which will get all content from IndexedDB.
-*/
 export const getDb = async () => {
-  // You can duplicate the same lines of code from above, except that the transaction will be 'readonly'
-  
-  // TODO: Copy LINES 28, 31 and 34 above; the new line 31 code should be "readonly"
+  const holdDB = await openDB(DB_jate, 1);
 
-  // Leave the rest as-is
-  const request = store.get(1);
+  const textVar = holdDB.transaction(DB_jate, 'readonly');
+
+  const storeVar = textVar.objectStore(DB_jate);
+
+  const request = storeVar.get(1);
   const result = await request;
   result
     ? console.log('🚀 - data retrieved from the database', result.value)
